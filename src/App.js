c95993985f42996e0d34b20e6013589bf19b01bc
nam <tln9815571@gmail.com>
2019-03-31 17:36:49 +0700

    add user list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React from 'react'
+const users=[{
+  firstName: 'Nam',
+  lastname: 'Tran',
+  email:'tln9815571@gmail.com',
+},
+{
+  firstName: 'Na',
+  lastname: 'Tra',
+  email:'tln981571@gmail.com',
+},
+{
+  firstName: 'm',
+  lastname: 'n',
 }
-
+]
+function getFullName(first, last)
+{
+  return first + " " + last
+}
+function User(props) {
+  const user=props.use
+   const elenment=user.email? <div><h1>{getFullName(user.firstName,user.lastname)}</h1> <div style={{color:'gray'}}>{user.email}</div></div>:<div><h1>{getFullName(user.firstName,user.lastname)}</h1> <div style={{color:'red'}}><i>khong co email</i></div></div>
+  
+ 
+  return elenment
+}
+function App(){
+const e=users.map(user =>{
+  return <User use={user} />
+})
+return e
+}
 export default App;
