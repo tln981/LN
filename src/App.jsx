3569de2d27759e432c252d6bf2b285eb0f76abd1
nam <tln9815571@gmail.com>
2019-04-14 17:44:56 +0700

    lifecycil

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
 import Counter from './Counter'
 
-function App() {
-  return <Counter />
+class App extends React.Component {
+  state = { showCounter: true, time: 500, detal: 10 }
+
+  handleToggleCounter = () =>
+    this.setState({ showCounter: !this.state.showCounter })
+  timeChange = event => {
+    this.setState({ time: event.target.value })
+  }
+  render() {
+    return (
+      <div>
+        {this.state.showCounter ? (
+          <div>
+            <Counter
+              name="Counter 1"
+              initialDelta={this.state.detal}
+              time={this.state.time}
+              initialautoIncreaseOn={false}
+            />
+            <Counter
+              name="Counter 2"
+              initialDelta={this.state.detal}
+              time={this.state.time}
+              initialautoIncreaseOn={true}
+            />
+            <input
+              type="text"
+              value={this.state.time}
+              onChange={this.timeChange}
+            />
+          </div>
+        ) : null}
+
+        <button onClick={this.handleToggleCounter}>
+          {this.state.showCounter ? 'Hide' : 'Show'} counter
+        </button>
+      </div>
+    )
+  }
 }
-export default App;
+
+export default App
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,48 +1,76 @@
 import React from 'react'
-import { isFulfilled } from 'q';
+import { isFulfilled } from 'q'
 
-
-const getColor=value=>{
-    if (value==0)
-    return "black"
-    if(value<0)
-    return "red"
-    if(value>0)
-    return "green"
+const getColor = value => {
+  if (value == 0) return 'black'
+  if (value < 0) return 'red'
+  if (value > 0) return 'green'
 }
-class Counter extends React.Component{
-    state={value :0,detal:0}
-    change=value=>{
-        const curentValue = this.state.value
-        this.setState({value:curentValue+value})   
+class Counter extends React.Component {
+  state = {
+    value: 0,
+    detal: this.props.initialDelta || 0,
+    autoIncreaseOn: this.props.initialautoIncreaseOn,
+  }
+  interval = null
+  change = value => {
+    const curentValue = this.state.value
+    this.setState({ value: curentValue + value })
+  }
+  changeDetal = value => {
+    const curentValue = this.state.detal
+    this.setState({ detal: curentValue + value })
+  }
+  reset = () => {
+    this.setState({ value: 0 })
+  }
+  autoIncrease = () => {
+    this.setState({ autoIncreaseOn: !this.state.autoIncreaseOn })
+    if (this.state.autoIncreaseOn) {
+      this.interval = setInterval(() => {
+        this.change(this.state.detal)
+      }, this.props.time)
+    } else {
+      clearInterval(this.interval)
     }
-    changeDetal=value=>{
-        const curentValue = this.state.detal
-        this.setState({detal:curentValue+value})   
-    }
-    reset=()=>{
-        this.setState({value:0})
-    }
-    render(){
-        const value =this.state.value
-        const detal =this.state.detal
-        return(
+  }
+
+  componentDidMount() {
+    this.autoIncrease()
+  }
+
+  render() {
+    const value = this.state.value
+    const detal = this.state.detal
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
         <div>
-            <div>current value: <span style={{color:getColor(value)}}>{value}</span></div>
-            <div>
-              <button onClick={()=>this.change(this.state.detal)}>Update</button><span>Ket Qua: {this.state.value+this.state.detal}</span>
-            </div>
-            <div>delta: <span style={{color:getColor(detal)}}>{detal}</span></div>
-            <div>
-              <button onClick={()=>this.changeDetal(1)}>Increase</button>
-              <button onClick={()=>this.changeDetal(-1)}>Decrease</button>
-            </div>
+          current value: <span style={{ color: getColor(value) }}>{value}</span>
         </div>
-      )
-    }
+        <div>
+          <button onClick={() => this.change(this.state.detal)}>
+            Update <span>({this.state.value + this.state.detal})</span>
+          </button>
+        </div>
+        <div>
+          delta: <span style={{ color: getColor(detal) }}>{detal}</span>
+        </div>
+        <div>
+          <button onClick={() => this.changeDetal(1)}>Increase</button>
+          <button onClick={() => this.changeDetal(-1)}>Decrease</button>
+        </div>
+        <div>
+          <button onClick={this.autoIncrease}>
+            {this.state.autoIncreaseOn ? 'start' : 'stop'} auto Increase
+          </button>
+        </div>
+      </div>
+    )
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 }
-
-
-
 
 export default Counter
