09e5c099cfc7948b5ab071886d3fef9b52c403e1
nam <tln9815571@gmail.com>
2019-04-21 17:34:35 +0700

    todo app v2

--- src/Component/Taskitem.jsx
+++ src/Component/Taskitem.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 export default class Taskitem extends React.Component {
+  handleToggleCheck = () => {
+    const currentChecked = this.props.checked
+    this.props.onToggleChecked(!currentChecked)
+  }
   render() {
     return (
       <div>
-        <input type="checkbox" />
-        {this.props.task}
+        <input
+          type="checkbox"
+          checked={this.props.checked}
+          onChange={this.handleToggleCheck}
+        />
+        {this.props.task.value}
+        <button onClick={() => this.props.onRemove(this.props.id)}>
+          remove
+        </button>
       </div>
     )
   }
--- src/todoapp.jsx
+++ src/todoapp.jsx
@@ -2,32 +2,54 @@ import React from 'react'
 import Taskitem from './Component/Taskitem'
 import Taskinput from './Component/Taskinput'
 export default class TodoApp extends React.Component {
-  state = { tasks: [], checked: [] }
+  state = { tasks: [] }
   handleTaskSubmit = task => {
     const currentTask = this.state.tasks
-    const nextTasks = currentTask.concat(task)
+    const nextTasks = currentTask.concat([
+      {
+        value: task,
+        checked: false,
+      },
+    ])
     this.setState({ tasks: nextTasks })
   }
-
-  handleChecked = task => {
-    const currentTask = this.state.checked
-    const nextTasks = currentTask.concat(task)
-    this.setState({ checked: nextTasks })
+  handleTaskToggleChecked = (taskIdx, checked) => {
+    const currentTasks = this.state.tasks
+    currentTasks[taskIdx].checked = checked
+    this.setState({ task: currentTasks })
+  }
+  handleRemove = idx => {
+    const newTasks = this.state.tasks.filter(
+      task => task !== this.state.tasks[idx],
+    )
+    this.setState({ tasks: newTasks })
   }
   render() {
+    const checkedTasksCount = this.state.tasks.filter(task => task.checked)
+      .length
     return (
       <div>
         <h2 style={{ marginBottom: 24 }}>Todo App</h2>
         <Taskinput onTaskSubmit={this.handleTaskSubmit} />
-        <div>
-          {this.state.checked.length}/{this.state.tasks.length}
-        </div>
+        {this.state.tasks.length ? (
+          <div>
+            Checked: {checkedTasksCount}/{this.state.tasks.length}{' '}
+            {checkedTasksCount === this.state.tasks.length ? 'Done!' : null}
+          </div>
+        ) : (
+          'No tasks yet!'
+        )}
         <div style={{ marginTop: 24 }}>
           {this.state.tasks.map((task, idx) => (
             <Taskitem
               task={task}
               key={idx}
-              onCheckedSubmit={this.handleChecked}
+              id={idx}
+              checked={task.checked}
+              onToggleChecked={checked =>
+                this.handleTaskToggleChecked(idx, checked)
+              }
+              onRemove={this.handleRemove}
             />
           ))}
         </div>
